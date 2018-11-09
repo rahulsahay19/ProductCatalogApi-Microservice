4ee6750d00ae9125ee237bfb6a0660c9eadfb85e
Rahul Sahay <rahulsahay19@hotmail.com>
2018-11-09 17:39:15 +0530

    Add project files.

---
+++ src/Services/ProductCatalogApi/Controllers/CatalogController.cs
@@ -0,0 +1,191 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using ProductCatalogApi.Data;
+using ProductCatalogApi.Domain;
+using ProductCatalogApi.ViewModels;
+
+namespace ProductCatalogApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CatalogController : ControllerBase
+    {
+        private readonly CatalogContext _catalogContext;
+        private readonly IOptionsSnapshot<CatalogSettings> _settings;
+
+        public CatalogController(CatalogContext catalogContext, IOptionsSnapshot<CatalogSettings> settings)
+        {
+            _catalogContext = catalogContext;
+            _settings = settings;
+            ((DbContext) catalogContext).ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public async Task<IActionResult> CatalogTypes()
+        {
+            var items = await _catalogContext.CatalogTypes.ToListAsync();
+            return Ok(items);
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public async Task<IActionResult> CatalogBrands()
+        {
+            var items = await _catalogContext.CatalogBrands.ToListAsync();
+            return Ok(items);
+        }
+        [HttpGet]
+        [Route("items/{id:int}")]
+        public async Task<IActionResult> GetItemById(int id)
+        {
+            if (id <= 0)
+            {
+                return BadRequest();
+            }
+
+            var item = await _catalogContext.CatalogItems.SingleOrDefaultAsync(c => c.Id == id);
+            if (item != null)
+            {
+                item.PictureUrl = item.PictureUrl.Replace("http://externalcatalogbaseurltobereplaced",
+                    _settings.Value.ExternalCatalogBaseUrl);
+                return Ok(item);
+            }
+
+            return NotFound();
+        }
+
+        //Get api/catalog/items[?pageSize=4&pageIndex=3]
+        [HttpGet]
+        [Route("[action]")]
+        public async Task<IActionResult> Items([FromQuery] int pageSize = 6, [FromQuery] int pageIndex = 0)
+        {
+            var totalItems = await _catalogContext.CatalogItems
+                .LongCountAsync();
+            var itemsOnPage = await _catalogContext.CatalogItems
+                .OrderBy(c => c.Name)
+                .Skip(pageSize * pageIndex)
+                .Take(pageSize)
+                .ToListAsync();
+
+            itemsOnPage = ChangeUrlPlaceHolder(itemsOnPage);
+
+            var model = new PaginatedItemsViewModel<CatalogItem>(pageIndex, pageSize, totalItems, itemsOnPage);
+            return Ok(model);
+        }
+        //Get api/catalog/items/withname/wonder?pageSize=2&pageIndex=0
+        [HttpGet]
+        [Route("[action]/withname/{name:minlength(1)}")]
+        public async Task<IActionResult> Items(string name, [FromQuery] int pageSize = 6, [FromQuery] int pageIndex = 0)
+        {
+            var totalItems = await _catalogContext.CatalogItems
+                .Where(c=>c.Name.StartsWith(name))
+                .LongCountAsync();
+            var itemsOnPage = await _catalogContext.CatalogItems
+                .Where(c=>c.Name.StartsWith(name))
+                .OrderBy(c => c.Name)
+                .Skip(pageSize * pageIndex)
+                .Take(pageSize)
+                .ToListAsync();
+
+            itemsOnPage = ChangeUrlPlaceHolder(itemsOnPage);
+
+            var model = new PaginatedItemsViewModel<CatalogItem>(pageIndex, pageSize, totalItems, itemsOnPage);
+            return Ok(model);
+        }
+
+        //Get api/catalog/items/type/1/brand/null[?pageSize=4&pageIndex=0]
+        [HttpGet]
+        [Route("[action]/type/{catalogTypeId}/brand/{catalogBrandId}")]
+        public async Task<IActionResult> Items(int? catalogTypeId, int? catalogBrandId, [FromQuery] int pageSize = 6, [FromQuery] int pageIndex = 0)
+        {
+            //Iqeryable means query is not ready yet and don't send this query to db yet
+            var root = (IQueryable<CatalogItem>)_catalogContext.CatalogItems;
+            if (catalogTypeId.HasValue)
+            {
+                root = root.Where(c => c.CatalogTypeId == catalogTypeId);
+            }
+
+            if (catalogBrandId.HasValue)
+            {
+                root = root.Where(c => c.CatalogBrandId == catalogBrandId);
+            }
+            var totalItems = await root
+                .LongCountAsync();
+            var itemsOnPage = await root
+                .OrderBy(c => c.Name)
+                .Skip(pageSize * pageIndex)
+                .Take(pageSize)
+                .ToListAsync();
+
+            itemsOnPage = ChangeUrlPlaceHolder(itemsOnPage);
+
+            var model = new PaginatedItemsViewModel<CatalogItem>(pageIndex, pageSize, totalItems, itemsOnPage);
+            return Ok(model);
+        }
+
+        //Post api/catalog/items/
+        [HttpPost]
+        [Route("items")]
+        public async Task<IActionResult> CreateProduct([FromBody]CatalogItem produt)
+        {
+            var item = new CatalogItem
+            {
+                CatalogBrandId = produt.CatalogBrandId,
+                CatalogTypeId = produt.CatalogTypeId,
+                Description = produt.Description,
+                Name = produt.Name,
+                PictureFileName = produt.PictureFileName,
+                Price = produt.Price
+            };
+
+            _catalogContext.CatalogItems.Add(item);
+            await _catalogContext.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetItemById), new {id = item.Id});
+        }
+
+        [HttpPut]
+        [Route("items")]
+        public async Task<IActionResult> UpdateProduct([FromBody] CatalogItem productToUpdate)
+        {
+            var catalogItem = await _catalogContext.CatalogItems
+                .SingleOrDefaultAsync(c => c.Id == productToUpdate.Id);
+
+            if (catalogItem == null)
+            {
+                return NotFound(new {Message = $"Item with id {productToUpdate.Id} not found"});
+            }
+
+            catalogItem = productToUpdate;
+            _catalogContext.CatalogItems.Update(catalogItem);
+            await _catalogContext.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetItemById), new { id = productToUpdate.Id });
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<IActionResult> DeleteProduct(int id)
+        {
+            var product = await _catalogContext.CatalogItems.SingleOrDefaultAsync(c => c.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            _catalogContext.CatalogItems.Remove(product);
+            await _catalogContext.SaveChangesAsync();
+            return NoContent();
+        }
+
+        private List<CatalogItem> ChangeUrlPlaceHolder(List<CatalogItem> items)
+        {
+            items.ForEach(x=>x.PictureUrl=x.PictureUrl.Replace("http://externalcatalogbaseurltobereplaced", _settings.Value.ExternalCatalogBaseUrl));
+            return items;
+        }
+    }
+}
---
+++ src/Services/ProductCatalogApi/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using ProductCatalogApi.Data;
+
+namespace ProductCatalogApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateWebHostBuilder(args).Build();
+
+            //From the scope, I will get the services
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    //Obtained an instance of catalog context
+                    var context = services.GetRequiredService<CatalogContext>();
+                    CatalogSeed.SeedAsync(context).Wait();
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex,"An error Occured while seeding values");
+                }
+            }
+            host.Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
