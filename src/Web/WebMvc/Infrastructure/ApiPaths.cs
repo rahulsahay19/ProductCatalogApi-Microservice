baf520fcb8ce7d96770a98aaf200aa95022c5fde
Rahul Sahay <rahulsahay19@hotmail.com>
2018-11-09 22:02:17 +0530

    Updated Infrastructure Code

---
+++ src/Web/WebMvc/Infrastructure/ApiPaths.cs
@@ -0,0 +1,40 @@
+﻿namespace EcommerceOnContainers.Web.WebMvc.Infrastructure
+{
+    public class ApiPaths
+    {
+
+        public static class Catalog
+        {
+            public static string GetAllCatalogItems(string baseUri, int page, int take, int? brand, int? type)
+            {
+                var filterQs = "";
+
+                if (brand.HasValue || type.HasValue)
+                {
+                    var brandQs = (brand.HasValue) ? brand.Value.ToString() : "null";
+                    var typeQs = (type.HasValue) ? type.Value.ToString() : "null";
+                    filterQs = $"/type/{typeQs}/brand/{brandQs}";
+                }
+
+                return $"{baseUri}items{filterQs}?pageIndex={page}&pageSize={take}";
+            }
+
+            public static string GetCatalogItem(string baseUri, int id)
+            {
+
+                return $"{baseUri}/items/{id}";
+            }
+            public static string GetAllBrands(string baseUri)
+            {
+                return $"{baseUri}catalogBrands";
+            }
+
+            public static string GetAllTypes(string baseUri)
+            {
+                return $"{baseUri}catalogTypes";
+            }
+        }
+
+
+    }
+}
--- src/Web/WebMvc/Startup.cs
+++ src/Web/WebMvc/Startup.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using EcommerceOnContainers.Web.WebMvc.Infrastructure;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -19,6 +20,8 @@ namespace EcommerceOnContainers.Web.WebMvc
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<AppSettings>(Configuration);
+            services.AddSingleton<IHttpClient, CustomHttpClient>();
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
