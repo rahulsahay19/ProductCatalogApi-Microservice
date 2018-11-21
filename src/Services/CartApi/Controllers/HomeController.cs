a47bb7d063ec9e967d8ca4ea3bdc7a68c221b6dd
Rahul Sahay <rahulsahay19@hotmail.com>
2018-11-22 00:00:50 +0530

    Updated swaager stuff in cart api

---
+++ src/Services/CartApi/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CartApi.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return new RedirectResult("~/swagger/ui");
+        }
+    }
+}
